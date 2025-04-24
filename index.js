b18fe67c8750f3e7f9a8c0d52bfcd0f13fef25f8
Niklas Hedström <niklashedstrom@Niklass-MacBook-Pro.local>
2025-04-24 19:02:42 +0200

    update

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
-const cron = require('node-cron');
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-});
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-
-  const today = new Date();
-  const sevenDaysLater = new Date();
-  sevenDaysLater.setDate(today.getDate() + 7);
-
-  const todayFormatted = formatDate(today);
-  const futureFormatted = formatDate(sevenDaysLater);
-
-  const url = `https://www.systembolaget.se/sortiment/ol/?saljstart-fran=${todayFormatted}&saljstart-till=${futureFormatted}`
-  let channel = client.channels.cache.get(process.env.CHANNEL_ID);
-  channel.send('🍺 Här är den kommande veckans öl på bolaget: ' + url);
-  
-
-  // Every Sunday at 10:00 AM
-  cron.schedule('0 10 * * 0', () => {
-    channel = client.channels.cache.get(process.env.CHANNEL_ID);
-    if (channel) {
-      channel.send('🍺 Här är den kommande veckans öl på bolaget: ' + url);
-    }
+async function main() {
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
   });
-});
 
-client.login(process.env.DISCORD_TOKEN);
+  await client.login(process.env.DISCORD_TOKEN);
+
+  client.once('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}`);
+
+    const today = new Date();
+    const sevenDaysLater = new Date();
+    sevenDaysLater.setDate(today.getDate() + 7);
+
+    const todayFormatted = formatDate(today);
+    const futureFormatted = formatDate(sevenDaysLater);
+
+    const url = `https://www.systembolaget.se/sortiment/ol/?saljstart-fran=${todayFormatted}&saljstart-till=${futureFormatted}`;
+    
+    const channel = await client.channels.fetch(process.env.CHANNEL_ID);
+    if (!channel) {
+      console.error('Channel not found');
+      process.exit(1);
+    }
+
+    await channel.send('🍺 Här är den kommande veckans öl på bolaget: ' + url);
+    console.log('Message sent!');
+
+    client.destroy();
+    process.exit(0); // Ensure a clean exit
+  });
+}
 
 function formatDate(date) {
   return date.toISOString().split('T')[0];
 }
 
+main().catch(err => {
+  console.error('Error in main:', err);
+  process.exit(1);
+});
